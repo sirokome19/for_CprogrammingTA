4797e318faeb586b2bcf97fc7eb04d469d8c20f9
sirokome19 <sirokome19@yahoo.co.jp>
2020-10-16 17:32:44 +0900

    for comparing file and math compile

--- for_Cprogramming_TA.sh
+++ for_Cprogramming_TA.sh
@@ -8,11 +8,18 @@ echo "$answer"
 
 ls $root_folder | while read LINE
 do
-    var=`gcc "$root_folder/$LINE/$answer_file_name" && \
+    var=`gcc  "$root_folder/$LINE/$answer_file_name" -lm && \
     cat ./${answer_file_name%.*}/input.txt | ./a.out && \
     rm a.out`
     if [ "$answer" != "$var" ] ; then
         echo "$root_folder/$LINE/$answer_file_name"
         echo "$var"
     fi
+
+    # =====to compare file
+    # if cat hoge.txt; then
+    #     rm hoge.txt
+    # else
+    #     echo "Abort\n"
+    # fi
 done
